0ffbf09fc7bf2532dce92f6b56cb3bb3436b47c6
かっしー <106821691+kasshii28@users.noreply.github.com>
2023-06-19 18:36:32 +0900

    add

--- backend/API/question_sub.py
+++ backend/API/question_sub.py
@@ -4,7 +4,6 @@ from flask import jsonify,Blueprint,request
 
 sys.path.append(os.path.join(os.path.dirname(__file__),'..'))
 import database as DB
-
 api_module = Blueprint("sub",__name__,url_prefix="/sub")
 
 def sub(question_sub_table):
